97d96e3faf15fd10a5dc8e9ae7f4c9aad613f522
Issi <alex.morosow1@gmail.com>
2020-08-27 22:11:01 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'wallpanel adapter settings': {
+		'en': 'Adapter settings for wallpanel',
+		'de': 'Adaptereinstellungen für wallpanel',
+		'ru': 'Настройки адаптера для wallpanel',
+		'pt': 'Configurações do adaptador para wallpanel',
+		'nl': 'Adapterinstellingen voor wallpanel',
+		'fr': "Paramètres d'adaptateur pour wallpanel",
+		'it': "Impostazioni dell'adattatore per wallpanel",
+		'es': 'Ajustes del adaptador para wallpanel',
+		'pl': 'Ustawienia adaptera dla wallpanel',
+		'zh-cn': 'wallpanel的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
