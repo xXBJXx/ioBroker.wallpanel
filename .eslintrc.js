a83835aedcb5e11136e480ee10fa94fd4e62ce1c
Issi <issi.dev.iobroker@gmail.com>
2022-05-29 20:50:07 +0200

    modifyd eslint for React and change printWidth to 110

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
 	root: true, // Don't look outside this project for inherited configs
-
 	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
 	parserOptions: {
 		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module', // Allows for the use of imports
 		project: './tsconfig.json',
+		ecmaFeatures: {
+			jsx: true,
+		},
 	},
 	extends: [
 		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
 		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+		'plugin:react/recommended', // Supports React JSX
 	],
-	plugins: ['prettier'],
+	plugins: ['react', 'prettier'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
 	rules: {
 		'@typescript-eslint/no-parameter-properties': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
@@ -46,13 +54,13 @@ module.exports = {
 		'prettier/prettier': [
 			'error',
 			{
-				printWidth: 160,
+				printWidth: 110,
 			},
 		],
 	},
 	overrides: [
 		{
-			files: ['*.test.ts'],
+			files: ['*.test.ts', '*.tsx'],
 			rules: {
 				'@typescript-eslint/explicit-function-return-type': 'off',
 			},
